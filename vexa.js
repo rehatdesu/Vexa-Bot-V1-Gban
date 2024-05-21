b809c8ba6840001cf0e838a16629235bf90a0794
rehatdesu <98342157+rehatdesu@users.noreply.github.com>
2024-05-21 18:34:00 +0600

    Create vexa.js

---
+++ vexa.js
@@ -0,0 +1,7 @@
+/*
+* Vexa V1 is a chat bot messenger launcher that allows you to create personal profiles or any other profiles you want to set up in the messnger bot. 
+* It is a free open-source project created by Allou Mohammed and Saad Al Saud, copyrighted in 2024 under the MIT License.
+* This GitHub repository is intended for developers who adhere to the project rules violators will be permanently banned from the project.
+* We have implemented extensive security measures in the source code. Anyone caught using spam or violating commands such as bullying or altering the author of commands will be permanently banned from the project.
+* Thank you.
+*/
